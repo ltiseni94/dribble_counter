3a747f05fee6177119dd003af498333fff0ce127
ltiseni94 <luca13tiseni@gmail.com>
2022-03-09 12:13:17 +0100

    modifications

--- testtrack.py
+++ testtrack.py
@@ -1,12 +1,16 @@
 import cv2
 import time
 import numpy as np
+import mediapipe as mp
 from collections import deque
 from optparse import OptionParser
 
+mp_drawing = mp.solutions.drawing_utils
+mp_drawing_styles = mp.solutions.drawing_styles
+mp_pose = mp.solutions.pose
+# protoFile = './pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt'
+# weightsFile = './pose/mpi/pose_iter_160000.caffemodel'
 
-protoFile = './pose/mpi/pose_deploy_linevec_faster_4_stages.prototxt'
-weightsFile = './pose/mpi/pose_iter_160000.caffemodel'
 
 def log(msg):
     print(f'[{time.strftime("%H:%M:%S")}]: {msg}')
@@ -20,6 +24,7 @@ def luca_count(q: deque, box: tuple):
     prev_avg = sum(np.diff(q))
     q.append(box[1] + box[3]/2)
     new_avg = sum(np.diff(q))
+    log(f'{new_avg} - {prev_avg} - {is_bounce(prev_avg, new_avg)}')
     return is_bounce(prev_avg, new_avg)
 
 
@@ -105,7 +110,7 @@ if __name__ == '__main__':
     else:
         tracker = cv2.TrackerMIL_create()
 
-    net: cv2.dnn_Net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
+    # net: cv2.dnn_Net = cv2.dnn.readNetFromCaffe(protoFile, weightsFile)
 
     video = cv2.VideoCapture('./resources/kaka_cut.mp4')
 
@@ -125,7 +130,7 @@ if __name__ == '__main__':
 
     cv2.namedWindow('first_frame', 1)
 
-    frame, key_points = update_dnn(net, frame, width, height)
+    # frame, key_points = update_dnn(net, frame, width, height)
 
     orig_frame = frame.copy()
     rectangle_corners = []
@@ -174,86 +179,103 @@ if __name__ == '__main__':
     bbox_centers_y.append((bbox[1] + round(bbox[3] / 2)))
 
     num_iter = 0
-    while True:
-        frame_read, frame = video.read()
-        if not frame_read:
-            log('No more frames available')
-            break
-        num_iter += 1
+    with mp_pose.Pose(
+        min_detection_confidence=0.5,
+        min_tracking_confidence=0.5
+    ) as pose:
+        while True:
+            frame_read, frame = video.read()
+            if not frame_read:
+                log('No more frames available')
+                break
+            num_iter += 1
 
-        # updating method + fps calculation
-        timer = cv2.getTickCount()
-        tracker_ok, bbox = tracker.update(frame)
-        # frame, key_points = update_dnn(net, frame, width, height)
-        fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
+            # updating method + fps calculation
+            timer = cv2.getTickCount()
+            tracker_ok, bbox = tracker.update(frame)
+            frame.flags.writeable = False
+            frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+            results = pose.process(frame)
+            frame.flags.writeable = True
+            frame = cv2.cvtColor(frame, cv2.COLOR_RGB2BGR)
+            mp_drawing.draw_landmarks(
+                frame,
+                results.pose_landmarks,
+                mp_pose.POSE_CONNECTIONS,
+                landmark_drawing_spec=mp_drawing_styles.get_default_pose_landmarks_style())
 
-        if tracker_ok:
-            # Draw bounding box
-            p1 = (int(bbox[0]), int(bbox[1]))
-            p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
-            cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
+            fps = cv2.getTickFrequency() / (cv2.getTickCount() - timer)
 
-            # Luca's method for bounce detection
-            luca_bounce += luca_count(luca_queue, bbox)
+            if tracker_ok:
+                # Draw bounding box
+                p1 = (int(bbox[0]), int(bbox[1]))
+                p2 = (int(bbox[0] + bbox[2]), int(bbox[1] + bbox[3]))
+                cv2.rectangle(frame, p1, p2, (255, 0, 0), 2, 1)
 
-            # Tommaso's method for bounce detection
-            bbox_centers_y.append((bbox[1] + round(bbox[3] / 2)))
-            if not bbox_centers_y[-1] == bbox_centers_y[-2]:  # evito "stazionamento" palla
-                delta.append(bbox_centers_y[-1] - bbox_centers_y[-2])
-            if len(delta) > 1:
-                if delta[1] < 0 and (delta[1] * delta[0]) < 0:
-                    # delta < 0 : risalita, (delta[1]*delta[0]) < 0 : cambio di segno
-                    tommaso_bounce += 1
+                # Luca's method for bounce detection
+                # bounce = luca_count(luca_queue, bbox)
+                # if bounce:
+                #     frame, key_points = update_dnn(net, frame, width, height)
+                luca_bounce += luca_count(luca_queue, bbox)
 
-        else:
+                # Tommaso's method for bounce detection
+                bbox_centers_y.append((bbox[1] + round(bbox[3] / 2)))
+                if not bbox_centers_y[-1] == bbox_centers_y[-2]:  # evito "stazionamento" palla
+                    delta.append(bbox_centers_y[-1] - bbox_centers_y[-2])
+                if len(delta) > 1:
+                    if delta[1] < 0 and (delta[1] * delta[0]) < 0:
+                        # delta < 0 : risalita, (delta[1]*delta[0]) < 0 : cambio di segno
+                        tommaso_bounce += 1
+
+            else:
+                cv2.putText(frame,
+                            "Tracking failure detected",
+                            (100, 80),
+                            cv2.FONT_HERSHEY_SIMPLEX,
+                            0.75,
+                            (0, 0, 255),
+                            2)
+
+            # Write tracker type
             cv2.putText(frame,
-                        "Tracking failure detected",
-                        (100, 80),
+                        f'{tracker_type} Tracker',
+                        (100, 20),
                         cv2.FONT_HERSHEY_SIMPLEX,
                         0.75,
-                        (0, 0, 255),
+                        (50, 170, 50),
                         2)
 
-        # Write tracker type
-        cv2.putText(frame,
-                    f'{tracker_type} Tracker',
-                    (100, 20),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.75,
-                    (50, 170, 50),
-                    2)
+            # Print FPS
+            cv2.putText(frame,
+                        f"FPS: {fps:.2f}",
+                        (100, 50),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75,
+                        (50, 170, 50),
+                        2)
 
-        # Print FPS
-        cv2.putText(frame,
-                    f"FPS: {fps:.2f}",
-                    (100, 50),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.75,
-                    (50, 170, 50),
-                    2)
+            # Print Tommaso's bounce
+            cv2.putText(frame,
+                        f"Bounce: {tommaso_bounce}",
+                        (100, 300),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75,
+                        (50, 170, 50),
+                        2)
 
-        # Print Tommaso's bounce
-        cv2.putText(frame,
-                    f"Bounce: {tommaso_bounce}",
-                    (100, 300),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.75,
-                    (50, 170, 50),
-                    2)
+            # Print Luca's bounce
+            cv2.putText(frame,
+                        f"Luca's bounce: {luca_bounce}",
+                        (100, 200),
+                        cv2.FONT_HERSHEY_SIMPLEX,
+                        0.75,
+                        (50, 170, 50),
+                        2)
 
-        # Print Luca's bounce
-        cv2.putText(frame,
-                    f"Luca's bounce: {luca_bounce}",
-                    (100, 200),
-                    cv2.FONT_HERSHEY_SIMPLEX,
-                    0.75,
-                    (50, 170, 50),
-                    2)
+            # Show image
+            cv2.imshow("Kaka", frame)
+            k = cv2.waitKey(1) & 0xff
+            if k == 27:
+                break
 
-        # Show image
-        cv2.imshow("Kaka", frame)
-        k = cv2.waitKey(1) & 0xff
-        if k == 27:
-            break
-
-    log(f'Processed {num_iter} frames. Bounces: {luca_bounce}')
+        log(f'Processed {num_iter} frames. Bounces: {luca_bounce}')
