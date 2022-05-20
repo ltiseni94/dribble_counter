13c97590c03d993eb62fc292bd8f7ca44cb36290
Tommaso Bagneschi <tommaso.bagneschi@gmail.com>
2022-05-20 16:52:17 +0200

    hsv
    da settare saturation and value limits

--- custom_trackers/hue_tracker.py
+++ custom_trackers/hue_tracker.py
@@ -34,11 +34,11 @@ class HueTracker:
         ilowH = self.low_h
         ihighH = self.high_h
 
-        ilowS = np.mean(bbox_hsv[:, :, 1]) - 20
+        ilowS = 0#np.mean(bbox_hsv[:, :, 1]) - 20
         ihighS = 255
 
-        ilowV = np.mean(bbox_hsv[:, :, 2]) - 20
-        ihighV = np.mean(bbox_hsv[:, :, 2]) + 20
+        ilowV = 0#np.mean(bbox_hsv[:, :, 2]) - 20
+        ihighV = 255#np.mean(bbox_hsv[:, :, 2]) + 20
         self.lower_hsv = np.array([ilowH, ilowS, ilowV])
         self.higher_hsv = np.array([ihighH, ihighS, ihighV])
 
