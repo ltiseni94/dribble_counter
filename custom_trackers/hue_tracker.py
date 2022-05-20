2f6a940f0633626da2c60aa29588a3da207e0f41
Tommaso Bagneschi <tommaso.bagneschi@gmail.com>
2022-05-20 16:47:46 +0200

    inrange HSV
    da finire

--- custom_trackers/hue_tracker.py
+++ custom_trackers/hue_tracker.py
@@ -4,14 +4,16 @@ from typing import List, Optional
 
 
 class HueTracker:
-    def __init__(self, mean_h: int = 90, delta_h: int = 20, min_number_points: int = 15):
+    def __init__(self, mean_h: int = 90, delta: int = 20, min_number_points: int = 15):
         self.bbox: Optional[List[int]] = None
         self.bbox_width: Optional[int] = None
         self.bbox_height: Optional[int] = None
         self.mean_h: int = mean_h
-        self.delta_h: int = delta_h
-        self.min_h: int = mean_h - delta_h
-        self.max_h: int = mean_h + delta_h
+        self.delta_h: int = delta
+        self.low_h: int = mean_h - delta
+        self.high_h: int = mean_h + delta
+        self.lower_hsv: np.ndarray = np.array([0, 0, 0])
+        self.higher_hsv: np.ndarray = np.array([0, 0, 0])
         self.min_number_points: int = min_number_points
         self.kernel: np.ndarray = cv2.getStructuringElement(cv2.MORPH_RECT, (10, 10))
 
@@ -24,9 +26,21 @@ class HueTracker:
         crop_bbox = image[self.bbox[1]:self.bbox[1]+self.bbox[3], self.bbox[0]:self.bbox[0]+self.bbox[2], :]
         #2: HUE
         bbox_hsv = cv2.cvtColor(crop_bbox, cv2.COLOR_BGR2HSV)
-        self.mean_h = np.mean(bbox_hsv[:, :, 0])         #forse è meglio la moda
-        self.min_h = (self.mean_h - self.delta_h)/2
-        self.max_h = (self.mean_h + self.delta_h)/2
+        self.mean_h = np.mean(bbox_hsv[:, :, 0])
+        self.low_h = (self.mean_h - self.delta_h)/2
+        self.high_h = (self.mean_h + self.delta_h)/2
+
+        #2bis: scala HSV
+        ilowH = self.low_h
+        ihighH = self.high_h
+
+        ilowS = np.mean(bbox_hsv[:, :, 1]) - 20
+        ihighS = 255
+
+        ilowV = np.mean(bbox_hsv[:, :, 2]) - 20
+        ihighV = np.mean(bbox_hsv[:, :, 2]) + 20
+        self.lower_hsv = np.array([ilowH, ilowS, ilowV])
+        self.higher_hsv = np.array([ihighH, ihighS, ihighV])
 
 
     def update(self, image: np.ndarray):
@@ -39,10 +53,13 @@ class HueTracker:
 
         #4: maschera su nuovo crop
         bbox_new_hsv = cv2.cvtColor(crop_bbox_new, cv2.COLOR_BGR2HSV)
-        if self.min_h > self.max_h:
-            mask = cv2.bitwise_not(cv2.inRange(bbox_new_hsv[:, :, 0], self.max_h, self.min_h))
-        else:
-            mask = cv2.inRange(bbox_new_hsv[:, :, 0], self.min_h, self.max_h)
+        #if self.low_h > self.high_h:
+            #mask = cv2.bitwise_not(cv2.inRange(bbox_new_hsv[:, :, 0], self.high_h, self.low_h))
+        #else:
+            #mask = cv2.inRange(bbox_new_hsv[:, :, 0], self.low_h, self.high_h)
+
+        mask = cv2.inRange(bbox_new_hsv, self.lower_hsv, self.higher_hsv)
+
         mask = cv2.erode(mask, self.kernel)
 
         #5: centro palla
@@ -54,6 +71,9 @@ class HueTracker:
 
         x_ball_image = x_ball_crop + self.bbox[0] - round(self.bbox[2]/2)
         y_ball_image = y_ball_crop + self.bbox[1] - round(self.bbox[3]/2)
+
+        #possibile raggio palla
+        #r_ball = (np.max(ball_indexes[1]).item() - np.min(ball_indexes[1]).item())/2
 
         #6: bbox aggiornata
         self.bbox = [
